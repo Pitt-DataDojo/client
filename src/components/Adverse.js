fb7bdb61994085e71cc82ff1df3ae737d1609af4
Zak Risha <zjrisha@gmail.com>
2018-10-13 15:52:40 -0400

    fix adverse

--- src/components/Adverse.js
+++ src/components/Adverse.js
@@ -3,15 +3,10 @@ import Drug from './Drug.js';
 import '../styles/Results.css';
 
 class Results extends Component {
-
-  //mpld3.draw_figure
-
-
   render() {
     return(
       <div id="Results">
         <Drug drugName = {this.props.drugName}/>
-        <div dangerouslySetInnerHTML={this.createMarkup()}></div>
       </div>
     )
   }
